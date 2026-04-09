613b59de59544fd7e3ef0e29e17195ec6ca2c695
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-04-09 15:53:14 +0200

    Ajout du dossier src/ qui contiendra tous les fichiers importants pour le bon fonctionnement du projet.

---
+++ src/main/java/fr/fms/SpringContactApplication.java
@@ -0,0 +1,35 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import fr.fms.entities.Contact;
+//import fr.fms.entities.Category;
+import fr.fms.dao.ContactRepository;
+//import fr.fms.dao.CategoryRepository;
+
+//import java.util.List;
+
+@SpringBootApplication
+public class SpringContactApplication implements CommandLineRunner{
+	@Autowired
+	private ContactRepository contactRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {		    
+		 System.out.println("=== Tous les articles ===");
+		 for (Contact a : contactRepository.findAll()) {
+		     System.out.println(a);
+		    }		 
+	
+		}
+		
+}
+
+
