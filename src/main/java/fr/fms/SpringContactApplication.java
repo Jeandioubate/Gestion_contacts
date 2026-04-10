3881cc4f9e86644509a60e43c0bc286d7f6c3ba7
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-04-10 07:56:22 +0200

    Modification du fichier SpringContactApplication avec une méthode pour afficher tous les contacts en console.

--- src/main/java/fr/fms/SpringContactApplication.java
+++ src/main/java/fr/fms/SpringContactApplication.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 import fr.fms.entities.Contact;
-import fr.fms.entities.Category;
+//import fr.fms.entities.Category;
 import fr.fms.dao.ContactRepository;
-import fr.fms.dao.CategoryRepository;
+//import fr.fms.dao.CategoryRepository;
 
-import java.util.List;
+//import java.util.List;
 
 @SpringBootApplication
 public class SpringContactApplication implements CommandLineRunner{
 	@Autowired
 	private ContactRepository contactRepository;
 	
-	@Autowired
-	private CategoryRepository categoryRepository;
+	//@Autowired
+	//private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringContactApplication.class, args);
@@ -26,37 +26,10 @@ public class SpringContactApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
-		// 🔹 1. Création des catégories
-	    Category personnel = categoryRepository.findByName("Personnel");
-	    if (personnel == null) {
-	        personnel = categoryRepository.save(new Category("Personnel"));
-	    }
-
-	    Category professionnel = categoryRepository.findByName("Professionnel");
-	    if (professionnel == null) {
-	        professionnel = categoryRepository.save(new Category("Professionnel"));
-	    }
-	    
-	    // 🔹 2. Création des contacts
-	    List<Contact> contacts = List.of(
-	        new Contact("DOMINIQUE", "Mathias", "mdom@gmail.com", "05.35.45.50.55", "Toulouse"),
-	        new Contact("BOULANGER", "Robert", "rbou@gmail.com", "05.15.20.25.30", "Toulouse"),
-	        new Contact("DIOUBATE", "Jean", "djean@gmail.com", "05.20.25.30.35", "Toulouse")
-	    );
-	    
-	 // 🔹 3. Association catégories
-	    contacts.get(0).setCategory(personnel);
-	    contacts.get(1).setCategory(professionnel);
-	    contacts.get(2).setCategory(personnel);
-	    
-	 // 🔹 4. Insertion sans doublon (plus simple)
-	    for (Contact c : contacts) {
-	        if (contactRepository.findByEmail(c.getEmail()).isEmpty()) {
-	            contactRepository.save(c);
-	        }
-	    }
-		
+		 System.out.println("=== Tous les contacts ===");
+		 for (Contact a : contactRepository.findAll()) {
+		     System.out.println(a);
+		    }		
 	}
 	
 }
